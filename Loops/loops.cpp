0a6682f72aafa42f1af538fef9a2fb2d860f0a91
Vamsi995 <saivamsi.ds123@gmail.com>
2020-11-26 18:03:37 +0530

    added loops

---
+++ Loops/loops.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int num;
+
+	cin >> num;
+
+
+	for(int i=0;i<10;i++) {
+	
+		cout << num << " x " << (i+1) << " = " << num*(i+1) << endl;
+	
+	}
+
+}
