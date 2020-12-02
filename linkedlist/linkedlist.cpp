0449cb8ce4801f5fb83382b997a9aa8223d22f64
Vamsi995 <saivamsi.ds123@gmail.com>
2020-12-02 15:40:48 +0530

    completed linked list

---
+++ linkedlist/linkedlist.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          //Complete this method
+          
+          if(head == NULL) {
+              head = (Node *)malloc(sizeof(Node));
+              head->data = data;
+              head->next = NULL;
+              return head;
+          }
+          else {
+              
+              Node *temp = head;
+              Node *cur = (Node *)malloc(sizeof(Node));
+                    cur->data = data;
+              
+              
+              while(temp->next != NULL) {
+                  
+                    temp = temp->next;
+              }
+              
+                        
+                    temp->next = cur;
+                    
+                    cur->next = NULL;
+              return head;
+          }
+          
+          
+        
+          
+          
+          
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
